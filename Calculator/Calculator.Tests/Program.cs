fb7a509f7eb2a53d146ad3f4787c744193cca981
CosmosBeautiful <cosmos.beautiful@tutamail.com>
2019-08-24 13:36:03 +0500

    create Projects

---
+++ Calculator/Calculator.Tests/Program.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Calculator.Tests
+{
+    [TestClass]
+    public class Program
+    {
+        [TestMethod]
+        public void TestMain()
+        {
+        }
+    }
+}
