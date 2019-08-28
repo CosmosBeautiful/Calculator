5044352d5e5adf5c4216958200a32f36b7a8c092
CosmosBeautiful <cosmos.beautiful@tutamail.com>
2019-08-28 18:53:47 +0500

    refactoring Tests

--- Calculator/Calculator.Tests/Functional/ValidationEquationTests.cs
+++ Calculator/Calculator.Tests/Functional/ValidationEquationTests.cs
@@ -14,41 +14,46 @@ namespace Calculator.Tests.Functional
         public void PreparationEquationPutSpaces()
         {
             // arrange
-            //string equation = "42+18";
-            //string exected = "42 + 18";
+            Dictionary<string, string> equations = new Dictionary<string, string>
+            {
+                { "42+18/( 6+12 /4 )", "42 + 18 / ( 6 + 12 / 4 )"},
+                { "42+18/(6+12/4)", "42 + 18 / ( 6 + 12 / 4 )"},
+                { "18/( 6 )", "18 / ( 6 )"},
+                { "( 4 -1) *2", "( 4 - 1 ) * 2"}
+            };
 
-            string equation = "42+18/(6+12/4)";
-            string exected = "42 + 18 / ( 6 + 12 / 4 )";
+            foreach (var equation in equations)
+            {
+                // act
+                string actualEquation = ValidationEquation.PreparationEquationPutSpaces(equation.Key);
 
-            //string equation = "18/( 6 )";
-            //string exected = "18 / ( 6 )";
-
-            // act
-            string actualElements = ValidationEquation.PreparationEquationPutSpaces(equation);
-
-            // assert
-            Assert.AreEqual(exected, actualElements);
+                // assert
+                Assert.AreEqual(equation.Value, actualEquation, $"{equation.Value} != {actualEquation}");
+            }
         }
 
         [TestMethod]
         public void IsValidate()
         {
             // arrange
-            //string equation = "42+18";
-            //string exected = "42 + 18";
+            Dictionary<string, bool> equations = new Dictionary<string, bool>
+            {
+                { "42 + 18 / ( 6 + 12 / 4 )", true},
+                { "12/6+10-( 5*(6+ 2))+ 27", true},
+                { "( 1*2 )/( 12/6 )", true},
+                { "(100 /5 +6 * 2+ (44- 22/ 10))+ (84* 56/(12/ 6 )+90-(5*5-1) )", true},
+                { "5 * 4) *65", false},
+                { "( 4 - 1 ) * 2x", false}
+            };
 
-            string equation = "(42+18)/(6+12/4)";
-            //string equation = "( 42 + 18 ) / ( 6 + 12 / 4 )";
-            bool exected = true;
+            foreach (var equation in equations)
+            {
+                // act
+                bool actualValid = ValidationEquation.IsValidate(equation.Key);
 
-            //string equation = "18/( 6 )";
-            //string exected = "18 / ( 6 )";
-
-            // act
-            bool actual = ValidationEquation.IsValidate(equation);
-
-            // assert
-            Assert.AreEqual(exected, actual);
+                // assert
+                Assert.AreEqual(equation.Value, actualValid, $"{equation.Value} != {actualValid}");
+            }
         }
     }
 }
--- Calculator/Calculator.Tests/Repositories/CalculatorRepository/CalculatorRepositoryTests.cs
+++ Calculator/Calculator.Tests/Repositories/CalculatorRepository/CalculatorRepositoryTests.cs
@@ -1,15 +1,14 @@
 ﻿using Calculator.Repositories;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
-using System.Text;
 
 namespace Calculator.Tests.Repositories.CalculatorRepositoryTests
 {
     [TestClass]
     public class CalculatorRepositoryTests
     {
-
+        //public TestContext TestContextInstance { get; set; }
         private ICalculatorRepository calculatorRepository;
 
         [TestInitialize]
@@ -18,30 +17,32 @@ namespace Calculator.Tests.Repositories.CalculatorRepositoryTests
             calculatorRepository = new CalculatorRepository();
         }
 
+        //[DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML", "|DataDirectory|/testDataEquation.xml", "Equation", DataAccessMethod.Sequential)]
         [TestMethod]
         public void SolveEquation()
         {
             // arrange
-            //string equation = "42 + 18 / ( 6 + 12 / 4 )";
-            //double expectedResult = 44;
+            //string equation = Convert.ToString(TestContextInstance.DataRow["equation"]);
 
-            //string equation = "42 + 18 * ( 12 / 6 + 10 - ( 5 * ( 6 + 2 ) ) + 27 ) / ( 6 + 12 / 4 ) + 100 / 20";
-            //double expectedResult = 45;
+            Dictionary<string, double> equations = new Dictionary<string, double>
+            {
+                { "42 + 18 / ( 6 + 12 / 4 )", 44},
+                { "42 + 18 * ( 12 / 6 + 10 - ( 5 * ( 6 + 2 ) ) + 27 ) / ( 6 + 12 / 4 ) + 100 / 20", 45},
+                { "12 / 6 + 10 - ( 5 * ( 6 + 2 ) ) + 27", -1},
+                { "( 5 * 2 ) / ( 12 / 6 )", 5},
+                { "( 100 / 5 + 62 * 2 + ( 44 - 22 / 10 ) ) + ( 84 * 56 / ( 12 / 6 ) + 90 - ( 5 * 5 - 1 ) )", 2603.8},
+                { "17 + 9 / ( 4 - 1 ) * 65", 212},
+                { "( 4 - 1 ) * 2", 6}
+            };
 
-            //string equation = "12 / 6 + 10 - ( 5 * ( 6 + 2 ) ) + 27";
-            //double expectedResult = -1;
+            foreach (var equation in equations)
+            {
+                // act
+                double actualResult = calculatorRepository.SolveEquation(equation.Key);
 
-            //string equation = "( 5 * 2 ) / ( 12 / 6 )";
-            //double expectedResult = 5;
-
-            string equation = "( 100 / 5 + 62 * 2 + ( 44 - 22 / 10 ) ) + ( 84 * 56 / ( 12 / 6 ) + 90 - ( 5 * 5 - 1 ) )";
-            double expectedResult = 2603.8;
-
-            // act
-            double actualResult = calculatorRepository.SolveEquation(equation);
-
-            // assert
-            Assert.AreEqual(expectedResult, actualResult);
+                // assert
+                Assert.AreEqual(equation.Value, actualResult, $"{equation.Value} != {actualResult}");
+            }
         }
     }
 }
